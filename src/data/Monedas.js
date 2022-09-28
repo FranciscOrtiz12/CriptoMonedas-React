5c8a87dc72e1f3452a3e094b2a39cd79255f2439
Francisco Ortiz Villarroel <fco.ortizv12@gmail.com>
2022-09-28 13:06:10 -0300

    Moneda Chilena agregada al select

--- src/data/Monedas.js
+++ src/data/Monedas.js
@@ -1,9 +1,11 @@
 const monedas = [
     {id: 'USD', nombre: 'Dolar de Estados Unidos'},
-    {id: 'MXN', nombre: 'Peso Mexicano'},
     {id: 'EUR', nombre: 'Euro'},
+    {id: 'CLP', nombre: 'Peso Chileno'},
+    {id: 'MXN', nombre: 'Peso Mexicano'},
     {id: 'GBP', nombre: 'Libra Esterlina'},
 ];
+
 
 export { 
     monedas,
